feef187308cf33396a058f14e81f3a3ab86ed6c0
RenoirTan <renoirtan2005@gmail.com>
2023-05-02 17:35:34 +0800

    main: help

--- src/minias/main.c
+++ src/minias/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <config.h>
+
+typedef struct args {
+    char *asm_fp;
+    char *obj_fp;
+} args_t;
+
+static void help(void) {
+    puts("minias " MINIISA_VERSION);
+    puts("");
+    puts("Assemble your MiniISA assembly files.");
+    puts("");
+    puts("Usage: minias <asmfile> <objfile>");
+    puts("");
+    puts("Arguments:");
+    puts("    asmfile  Path to assembly file.");
+    puts("    objfile  Path to output object file.");
+}
 
 int main(int argc, char **argv) {
-    puts("minias");
+    if (argc != 3) {
+        help();
+        exit(1);
+    }
+    args_t *args = malloc(sizeof(args_t));
+    if (!args) {
+        fputs("Could not allocate memory for args.", stderr);
+    }
+    args->asm_fp = argv[1];
+    args->obj_fp = argv[2];
     return 0;
 }
