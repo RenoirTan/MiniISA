0d5a5efc0b7487251d4a41ab14d7b350964f89f7
RenoirTan <renoirtan2005@gmail.com>
2023-06-08 18:51:49 +0800

    main: make the declarations in instruction.h more portable

--- include/miniisa/instruction.h
+++ include/miniisa/instruction.h
@@ -3,58 +3,58 @@
 
 #   include <stdint.h>
 
-typedef int16_t register_id_t;
-typedef uint8_t register_type_t;
-typedef uint8_t register_size_t;
+typedef int16_t miniisa_register_id_t;
+typedef uint8_t miniisa_register_type_t;
+typedef uint8_t miniisa_register_size_t;
 
-typedef struct cpu_register {
-    register_id_t id;
-    register_type_t type;
-    register_size_t size;
-} cpu_register_t;
+typedef struct miniisa_register {
+    miniisa_register_id_t id;
+    miniisa_register_type_t type;
+    miniisa_register_size_t size;
+} miniisa_register_t;
 
-typedef int32_t mnemonic_t;
+typedef int32_t miniisa_mnemonic_t;
 
-typedef struct instruction {
-    mnemonic_t mnemonic;
-    cpu_register_t reg_a;
-    cpu_register_t reg_b;
+typedef struct miniisa_instruction {
+    miniisa_mnemonic_t mnemonic;
+    miniisa_register_t reg_a;
+    miniisa_register_t reg_b;
     uint8_t extra[8];
-} instruction_t;
+} miniisa_instruction_t;
 
-#   define NOP_MNEMONIC 0
-#   define ADD_MNEMONIC 1
-#   define SUB_MNEMONIC 2
-#   define MUL_MNEMONIC 3
-#   define DIV_MNEMONIC 4
-#   define AND_MNEMONIC 5
-#   define IOR_MNEMONIC 6
-#   define XOR_MNEMONIC 7
-#   define NOT_MNEMONIC 8
-#   define SHL_MNEMONIC 9
-#   define SAL_MNEMONIC 10
-#   define SHR_MNEMONIC 11
-#   define SAR_MNEMONIC 12
-#   define ROL_MNEMONIC 13
-#   define ROR_MNEMONIC 14
-#   define SET_MNEMONIC 15
-#   define MOV_MNEMONIC 16
-#   define LDR_MNEMONIC 17
-#   define STR_MNEMONIC 18
-#   define PSH_MNEMONIC 19
-#   define POP_MNEMONIC 20
-#   define XCG_MNEMONIC 21
-#   define CVT_MNEMONIC 22
-#   define CMP_MNEMONIC 23
-#   define JZR_MNEMONIC 24
-#   define JNZ_MNEMONIC 25
-#   define JEQ_MNEMONIC 26
-#   define JNE_MNEMONIC 27
-#   define JLT_MNEMONIC 28
-#   define JLE_MNEMONIC 29
-#   define JGT_MNEMONIC 30
-#   define JGE_MNEMONIC 31
-#   define INT_MNEMONIC 32
-#   define HLT_MNEMONIC 33
+#   define MINIISA_NOP 0
+#   define MINIISA_ADD 1
+#   define MINIISA_SUB 2
+#   define MINIISA_MUL 3
+#   define MINIISA_DIV 4
+#   define MINIISA_AND 5
+#   define MINIISA_IOR 6
+#   define MINIISA_XOR 7
+#   define MINIISA_NOT 8
+#   define MINIISA_SHL 9
+#   define MINIISA_SAL 10
+#   define MINIISA_SHR 11
+#   define MINIISA_SAR 12
+#   define MINIISA_ROL 13
+#   define MINIISA_ROR 14
+#   define MINIISA_SET 15
+#   define MINIISA_MOV 16
+#   define MINIISA_LDR 17
+#   define MINIISA_STR 18
+#   define MINIISA_PSH 19
+#   define MINIISA_POP 20
+#   define MINIISA_XCG 21
+#   define MINIISA_CVT 22
+#   define MINIISA_CMP 23
+#   define MINIISA_JZR 24
+#   define MINIISA_JNZ 25
+#   define MINIISA_JEQ 26
+#   define MINIISA_JNE 27
+#   define MINIISA_JLT 28
+#   define MINIISA_JLE 29
+#   define MINIISA_JGT 30
+#   define MINIISA_JGE 31
+#   define MINIISA_INT 32
+#   define MINIISA_HLT 33
 
 #endif
