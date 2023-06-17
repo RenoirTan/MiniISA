504a1e9802e663b33b5aa108b90133b2d7844b8e
RenoirTan <renoirtan2005@gmail.com>
2023-06-17 19:42:09 +0800

    main: new parser struct

--- src/minias/parser.c
+++ src/minias/parser.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <miniisa/instruction.h>
 #include "parser.h"
 #include "token.h"
@@ -9,6 +10,9 @@ parser_t *init_parser(parser_t *p) {
         if (!p) return NULL;
     }
     init_token(&p->prev_token);
+    p->state = PARSER_INIT;
     miniisa_instruction_init(&p->instruction);
+    memset(p->data, '\0', PARSER_DATA_MAX_LEN);
+    p->data_len = 0;
     return p;
 }
--- src/minias/parser.h
+++ src/minias/parser.h
@@ -1,12 +1,28 @@
 #ifndef MINIAS_PARSER_H
 #   define MINIAS_PARSER_H
 
+#   include <stddef.h>
 #   include <miniisa/instruction.h>
 #   include "token.h"
 
+#   define PARSER_DATA_MAX_LEN (1024)
+
+typedef enum parser_state {
+    PARSER_INIT = 0,
+    PARSER_DONE = 1,
+    PARSER_GETTING_NAME = 2,
+    PARSER_DETECTING_TYPE = 3,
+    PARSER_SETTING_DATA = 4,
+    PARSER_FINDING_ARGUMENT = 5,
+    PARSER_WAITING_COMMA = 6
+} parser_state_t;
+
 typedef struct parser {
     token_t prev_token;
+    parser_state_t state;
     miniisa_instruction_t instruction;
+    uint8_t data[PARSER_DATA_MAX_LEN];
+    size_t data_len;
 } parser_t;
 
 parser_t *init_parser(parser_t *p);
